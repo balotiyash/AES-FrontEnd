17529785c73a3a089a8a6a8806f27567cb991067
atharv0905 <atharvmirgal09@gmail.com>
2024-10-26 21:41:11 +0530

    minor changes for email verify script

--- user/controller/emailVerifyScript.js
+++ user/controller/emailVerifyScript.js
@@ -13,6 +13,7 @@ async function verifyEmail() {
     const currentUrl = window.location.href;
     const url = new URL(currentUrl);
     const token = url.searchParams.get('token');
+    console.log(token);
 
     if (token === null) {
         alert("Please login to send E-mail to the seller.");
@@ -20,18 +21,21 @@ async function verifyEmail() {
     }
 
     try {
-        const response = await fetch(`http://${IP}:${PORT}/email/verify`, {
+        const formdata = new FormData();
+        formdata.append('email', "atharv.mirgal@somaiya.edu");
+        formdata.append('token', token);
+        const response = await fetch(`http://${IP}:${PORT}/public/verify`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
+            },
+            body: formdata,
         });
 
         if (!response.ok) {
             throw new Error('Authentication failed');
         } else {
-            console.log("emil verified successfully");
+            console.log("Email verified successfully");
         }
     } catch (error) {
         console.error('Error during authentication:', error);
